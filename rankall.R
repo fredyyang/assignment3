8d1e189dfafbe92cb3140c7048695b40b20f3077
fredyyang <yangzhihao4023@gmail.com>
2014-05-19 17:06:00 +0800

    1

--- rankall.R
+++ rankall.R
@@ -1,11 +1,16 @@
 rankall <- function(outcome, num = "best") {
-      source("rankhospital.R")
       measureAll<-read.csv("outcome-of-care-measures.csv")
-      stateWithNum<-as.data.frame(table(measureAll[,"State"]),)
+      ##stateWithNum<-as.data.frame(table(measureAll[,"State"]),)
+      ##stateWithNum<-stateWithNum[order(stateWithNum[,1]),]
+      ##stateWithNum
+      ##class(stateWithNum[[1,2]])
       stateVectorFactor<-as.data.frame(table(measureAll[,"State"]))[,1]
       stateVector<-as.character(stateVectorFactor)
       stateVector<-sort(stateVector)
+      ##stateVector
+      outcome<-"heart attack"
       outcomeColumn<-0
+      num=10
       if(outcome=="heart attack")
       {
             outcomeColumn<-11
@@ -20,9 +25,22 @@ rankall <- function(outcome, num = "best") {
       {
             stop("invalid outcome")
       }
-      
-      
-      lapply(stateVector,print)
+      i<-1
+      hospitalVector<-NULL
+      while(i<=length(stateVector))
+      {
+            
+            measureState<-subset(measureAll,State==stateVector[i])
+            if(num=="best"){
+                  num=1
+            }else if(num=="worst"){
+                  num=sum(complete.cases(as.double(as.character(measureState[,outcomeColumn]))))
+            }
+            measureOrder<-measureState[order(as.double(as.character(measureState[,outcomeColumn])),measureState[,2]),]
+            hospital<-as.character(measureOrder[num,2])
+            hospital
+            i<-i+1
+      }
 
 
 }
